1e11203d81a530dcba64b00d9cd233c2e77ea733
Alexander Bryant <asbryant@me.com>
2025-01-19 16:14:21 -0700

    Splitting logic into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import HomePage from './components/HomePage';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className='min-h-screen flex flex-col'>
-        <header className='flex items-center justify-between gap-4 p-4'>
-          <h1>Free<span className='text-blue-400'>Scribe</span></h1>
-          <button className='flex items-center gap-2'>
-            <p>New</p>
-            <i className="fa-solid fa-plus"></i>
-          </button>
-        </header>
-        <main className='flex-1 p-4 flex flex-col justify-center'>
-          
-        </main>
+        <Header />
+        <HomePage />
       </section>
       <h1 className="text-green-400">Hello World.</h1>
       <footer>
