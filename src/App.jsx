6c8fc3c756097df1f26d66c1e486a3cc002e86bd
Alexander Bryant <asbryant@me.com>
2025-01-19 16:44:58 -0700

    First logic for renderings with file
    If audio file or audio stream is present, changed rendering, if none are present, homepage is rendered

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import HomePage from './components/HomePage';
 import Header from './components/Header';
+import FileDisplay from './components/FileDisplay';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+
+  const isAudioAvailable = file || audioStream
 
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className='min-h-screen flex flex-col'>
         <Header />
-        <HomePage />
+        {isAudioAvailable ? (
+          <FileDisplay />
+        ) : ( 
+          <HomePage />
+        )}
       </section>
       <h1 className="text-green-400">Hello World.</h1>
       <footer>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,11 +9,16 @@ function HomePage() {
             <h3 className='font-medium md:text-lg'>Record <span 
             className='text-blue-400'>&rarr;</span> Transcribe <span 
             className='text-blue-400'>&rarr;</span>Translate</h3>
-            <button className='flex items-center text-base justify-between 
-            gap-4 mx-auto w-72 max-w-full my-4'>
-                <p>Record</p>
+            <button className='flex specialBtn px-4 py-2 rounded-xl items-center 
+            text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+                <p className='text-blue-400'>Record</p>
                 <i className="fa-solid fa-microphone"></i>
             </button>
+            <p className='text-base'>Or <label className='text-blue-400 cursor-pointer 
+            hover:text-blue-600 duration-200'>Upload <input 
+            className='hidden' type='file' accept='.mp3,.wave' /></label>
+             an mp3 file</p>
+             <p className='italic text-slate-500'>Free now, free forever!</p>
         </main>
     )
 };
