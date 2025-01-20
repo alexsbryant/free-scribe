586cb4dff092ae587dbaab88aadbe886e1efea47
Alexander Bryant <asbryant@me.com>
2025-01-20 14:10:13 -0700

    Transcribing page style and animatiuon
    Updates the Transcribing page's margins, and added animation to index.css

--- src/components/Transcribing.jsx
+++ src/components/Transcribing.jsx
@@ -4,8 +4,8 @@ function Transcribing(props) {
     const { downloading } = props;
 
     return (
-        <div className='flex items-center flex-col justify-center
-        gap-10 md:gap-14 py-24'>
+        <div className='flex items-center flex-1 flex-col justify-center
+        gap-10 md:gap-14 pb-24 p-4 text-center'>
             <div className='flex flex-col gap-2 sm:gap-4'>
                 <h1 className='font-semibold text-4xl sm:text-5xl 
                 md:text-6xl'>
